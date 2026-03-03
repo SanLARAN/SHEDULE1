91cd63f10c1f4c40fd13ab275d39b3f754b27ffe
SanLARAN <136124874+SanLARAN@users.noreply.github.com>
2026-03-03 22:50:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ЗАМЕНИ "school-schedule" на ТОЧНОЕ имя твоего GitHub-репозитория
 export default defineConfig({
   plugins: [react()],
-  base: "/SHEDULE1/",
+  base: "./",
 });
