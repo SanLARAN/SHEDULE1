c769549560afa4597977b4fdc124e54f1f44bb99
SanLARAN <136124874+SanLARAN@users.noreply.github.com>
2026-03-03 22:51:59 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  -webkit-tap-highlight-color: transparent;
-}
-
-html,
-body,
-#root {
-  width: 100%;
-  min-height: 100vh;
-  min-height: 100dvh;
-  overflow-x: hidden;
-  background: #050816;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-body {
-  overscroll-behavior-y: none;
-}
-
-::-webkit-scrollbar {
-  display: none;
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
